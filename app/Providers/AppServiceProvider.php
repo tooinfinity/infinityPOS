73d6cf9086fd5aadbbb264779ce2a7c8264ec5ef
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2026-02-03 23:28:16 +0100

    refactor: remove unused model and password defaults from provider, add essentials configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -17,17 +15,6 @@ final class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->bootModelsDefaults();
-        $this->bootPasswordDefaults();
-    }
-
-    private function bootModelsDefaults(): void
-    {
-        Model::unguard();
-    }
-
-    private function bootPasswordDefaults(): void
-    {
-        Password::defaults(fn () => app()->isLocal() || app()->runningUnitTests() ? Password::min(12)->max(255) : Password::min(12)->max(255)->uncompromised());
+        //
     }
 }
